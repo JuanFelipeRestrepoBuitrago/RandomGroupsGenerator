7c000615d5bbe62a7cebb6228d31d3ca64082341
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-07-15 09:04:24 -0500

    Dropdown Function
    The dropdwon now appears in the screen when a button is clicked

---
+++ script.js
@@ -0,0 +1,20 @@
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+const dropdownLink = document.querySelector('.dropdown-link');
+
+const changeLanguage = document.querySelector('#change-language');
+
+function toggleDropdown(element) {
+    element.classList.toggle('hidden');
+}
+
+document.addEventListener('click', function(event) {
+    if (!dropdown.contains(event.target) && !dropdownContent.classList.contains('hidden')) {
+        dropdownContent.classList.add('hidden');
+    }
+});
+
+changeLanguage.addEventListener('change', function(event) {
+    console.log(event.target.value);
+});
+dropdownLink.addEventListener('click',  toggleDropdown.bind(null, dropdownContent));
